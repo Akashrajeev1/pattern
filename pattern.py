c003425a8ccc32bd9c62d1a5b8d2789b98626d94
Akashrajeev1 <110896835+Akashrajeev1@users.noreply.github.com>
2022-08-29 14:46:39 +0530

    upload

---
+++ pattern.py
@@ -0,0 +1,28 @@
+# for i in range(0,5):
+#     for j in range(0,i+1):
+#         print("* ",end="")      #end="" is used to contn in a line
+#     print("\r")
+
+# for i in range(1,6):
+#     for j in range(i):
+#         print(i, end="")  # end="" is used to contn in a line
+#     print("\r")
+
+#
+# for i in range(1,6):
+#     for j in range(i+1):
+#         print(j, end="")  # end="" is used to contn in a line
+#     print("\r")
+
+# b=0
+# for i in range(5,0,-1):
+#     b+=1
+#     for j in range(1,i+1):
+#         print(b, end="")  # end="" is used to contn in a line
+#     print("\r")
+
+
+for i in range(5,0,-1):
+    for j in range(0,i):
+        print(10, end="")  # end="" is used to contn in a line
+    print("\r")
